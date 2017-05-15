4b55e536d4da2e876bcb2ae9f862bf3604d1e749
Zhang <yzhang7@corp.ad.wrs.com>
2017-05-15 14:59:27 +0800

    Modify code with PEP8

--- check_price.py
+++ check_price.py
@@ -4,59 +4,65 @@ import pymysql.cursors
 from prettytable import PrettyTable
 from colorama import init, Fore
 import pdb
+import sys
 
+
+#数据库名称
 database_name = "house_price_04"
-
 # 打开数据库连接
-db=pymysql.connect("localhost","root","aB123456",database_name,charset='utf8mb4')
+db = pymysql.connect("localhost","root","aB123456",database_name,charset='utf8mb4')
 # 使用cursor()方法获取操作游标
-cursor=db.cursor()
-#输入要查询的小区名称
-
-data=[]
+cursor = db.cursor()
 
 def main():
+	#用于存储查询到包含关键字的小区信息check
+	global data
+	data = []
 
 	global check_name 
-	check_name= input("请输入小区名称：");
-	#用于存储查询到包含关键字的小区信息
+	
+	count = 10
+	while count > 1 :
+		#输入要查询的小区名称
+		check_name = input("请输入小区名称：");
+		
+		if check_name == 'exit' or check_name == 'quit':
+			sys.exit()
 
-	header = '地区 id 小区名称 价格 在售'.split()
-	pt = PrettyTable()
-	pt._set_field_names(header)
+		# pdb.set_trace() # 运行到这里会自动暂停
 
-	#获取所有table
-	tables=show_tables()
-	for table in tables:
-		select_info(table)
+		header = '地区 id 小区名称 价格 在售'.split()
+		pt = PrettyTable()
+		pt._set_field_names(header)
 
+		#获取所有table
+		tables = show_tables()
+		for table in tables:
+			select_info(table)
 
+		for row in data:
+			# row_list=list(row)
+			new_row = [
+					Fore.GREEN + row[0] + Fore.RESET,
+					row[1],
+					Fore.GREEN + row[2] + Fore.RESET,
+					Fore.RED + str(row[3]) + Fore.RESET,
+					row[4],
+			]
+			pt.add_row(new_row)
 
-	for row in data:
-		# row_list=list(row)
-		new_row=[
-				Fore.GREEN + row[0] + Fore.RESET,
-				row[1],
-				Fore.GREEN + row[2] + Fore.RESET,
-				Fore.RED + str(row[3]) + Fore.RESET,
-				row[4],
-		]
-		pt.add_row(new_row)
-
-	print(pt)
-
-
+		print(pt)
+		count = count - 1
+		data = []
 
 def show_tables():
-	sql="show tables;"
+	sql = "show tables;"
 	try:
 		cursor.execute(sql)
 		tables=cursor.fetchall()
 	except:
 		print ("Error: unable to fetch table data")
 	return tables
-
-
 
 def select_info(table):
 	sql = "SELECT * FROM %s;" % table
@@ -66,14 +72,13 @@ def select_info(table):
 	   # 获取所有记录列表
 	   results = cursor.fetchall()
 	   for row in results:
-	   		name=row[1]
+	   		name = row[1]
 	   		if(check_name in name):
 	   			area= table[0]
 	   			rowList= list(row)
-	   			rowList.insert(0,area)
+	   			rowList.insert(0, area)
 	   			
 	   			data.append(rowList)
-
 
 	except:
 	   print ("Error: unable to 小区 data")
--- crawler_lianjia.py
+++ crawler_lianjia.py
@@ -4,8 +4,8 @@ import requests
 import time
  
 homepage = 'http://bj.lianjia.com/xiaoqu/'
-areas=['fengtai','shijingshan','changping','daxing','shunyi',    \
-		'fangshan','mentougou','pinggu','huairou','miyun','haidian','yanqing']
+areas = ['fengtai', 'shijingshan', 'changping', 'daxing', 'shunyi',    \
+		'fangshan', 'mentougou', 'pinggu', 'huairou', 'miyun', 'haidian', 'yanqing']
 
 # areas=['miyun','dongcheng','tongzhou','xicheng','chaoyang',     \
 		# 'fengtai','shijingshan','changping','daxing','shunyi',    \
@@ -26,32 +26,32 @@ header_info = {
 
 def main():
 	for area in areas:
-		area_data=[]
-		totalPage=get_area_info(area)
+		area_data = []
+		totalPage = get_area_info(area)
 		time.sleep(10)
-		pages =[i for i in range(int(totalPage[0][0])+1)][1:]
+		pages = [i for i in range(int(totalPage[0][0])+1)][1:]
 		for page in pages:
-			xiaoquList=get_xiaoqu_info(homepage+area+'/pg'+str(page)+'/')
+			xiaoquList = get_xiaoqu_info(homepage+area+'/pg'+str(page)+'/')
 			area_data.extend(xiaoquList)
 			time.sleep(10)
 		write_to_file(area,area_data)
 
 def get_area_info(area):
-	r = requests.get(homepage+area, verify=False,headers=header_info)
+	r = requests.get(homepage+area, verify= False , headers = header_info)
 	totalPage_pattern = re.compile('totalPage\":(\d*),\"curPage\":(\d*)}',re.S)
-	totalPage = re.findall(totalPage_pattern,r.text)
+	totalPage = re.findall(totalPage_pattern, r.text)
 	return totalPage
 
 def get_xiaoqu_info(url):
-	request=requests.get(url,verify=False,headers=header_info)
+	request=requests.get(url, verify = False, headers = header_info)
 	xiaoqu_pattern=re.compile('clear\sxiaoquListItem.*?info.*?title.*?_blank\">(.*?)'+
 			'<\/a.*?totalPrice.*?<span>(\d*)<\/span>.*?totalSellCount.*?<span>(\d*)<\/span>',re.S)
-	xiaoquList = re.findall(xiaoqu_pattern,request.text)   #获取该页面小区信息
+	xiaoquList = re.findall(xiaoqu_pattern, request.text)   #获取该页面小区信息
 	print(xiaoquList)
 	return xiaoquList
 
-def write_to_file(area_name,area_data):
-	with open(area_name +'.txt','w', encoding="utf-8") as f:
+def write_to_file(area_name, area_data):
+	with open(area_name +'.txt','w', encoding = "utf-8") as f:
 		for data in area_data:
 			f.write('  '.join(data)+'\n')
 
